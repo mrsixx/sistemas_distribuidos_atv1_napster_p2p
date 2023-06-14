c64f583f21630a81231a9646eb5c5880e3efb9f4
Matheus Ribeiro <matheus1999201000@gmail.com>
2023-06-14 23:48:03 +0000

    feature: implementação de menu iterativo e listenner para solicitações de download com threads (features não bloqueantes); implementação de captura das informações via teclado;
    refactor: melhorias em nomenclaturas

--- client.py
+++ client.py
@@ -1,16 +1,22 @@
 import socket
 from dataclasses import dataclass
 import command as cmd
+from threading import Thread
 from mysocket import receive_all
+from time import sleep
 
 @dataclass
 class Client:
-    def __init__(self, ip: str, port: int) -> None:
+    def __init__(self, ip: str, port: int, path: str) -> None:
         self._ip = ip
         self._port = port
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((self.ip, self.port))
-
+        self._path = path
+        self._server_ip = '127.0.0.1'
+        self._server_port = 1099
+        # criação do socket para atender requisições de download e bind com ip:porta parametrizado
+        self._download_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._download_socket.bind((self.ip, self.port))
+        self._download_socket.listen(5)
     # region getters
     @property
     def ip(self) -> str:
@@ -19,46 +25,136 @@ class Client:
     @property
     def port(self) -> int:
         return self._port
-
+    
     @property
-    def socket(self):
-        return self._socket
+    def path(self) -> str:
+        return self._path
+    
+    @property
+    def server_ip(self) -> str:
+        return self._server_ip
+    
+    @property
+    def server_port(self) -> int:
+        return self._server_port
+    
+    @property
+    def download_socket(self) -> socket.socket:
+        return self._download_socket
     # endregion
 
-    def send_request(self, request: str) -> str:
-        self.socket.sendall(request.encode())
-        response = receive_all(self.socket)
+    # region funções de comunicação com o servidor
+    def send_request(self, socket: socket.socket, request: str) -> str:
+        socket.sendall(request.encode())
+        response = receive_all(socket)
         return response
+    
+    def open_server_connection(self) -> socket.socket:
+        sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sk.connect((self.server_ip, self.server_port))
+        return sk
 
-    def close(self):
-        self.socket.close()
+    def close_server_connection(self, socket: socket.socket) -> None:
+        socket.close()
+    # endregion
 
+    # region features
     def join(self) -> None:
-        #TODO: obter nome pasta e buscar nome dos arquivos disponiveis
-        command = cmd.join_command('ablu.mp4', 'shazam.mp3')
-        response = self.send_request(command)
-        if response != '':
-            cmd.join_ok_command_handler()
+        try:
+            # abre-se uma conexão com o servidor
+            conn = self.open_server_connection()
+            #TODO: obter nome pasta e buscar nome dos arquivos disponiveis
+            command = cmd.join_command('ablu.mp4', 'shazam.mp3')
+            response = self.send_request(conn, command)
+            if response != '':
+                cmd.join_ok_command_handler()
+        finally:
+            self.close_server_connection(conn)
 
     def search(self) -> None:
         print('search feature')
 
     def download(self) -> None:
         print('download feature')
+    
+    # endregion
 
-def main():
-    client = Client('127.0.0.1', int(input('server port:')))
-    try:
+    def listen_download_requests(self) -> None:
         while True:
-            option = int(input('1 - JOIN | 2 - SEARCH | 3 - DOWNLOAD: '))
-            if option == 1:
-                client.join()
-            elif option == 2:
-                client.search()
-            elif option == 3:
-                client.download()
-    finally:
-        client.close()
+            # aguardo um outro peer se conectar
+            peer_socket, peer_address = self.download_socket.accept()
+            # despacho para um thread tratar sua requisição
+            handler_thread = self.DownloadRequestHandlerThread(self, peer_socket, f'{peer_address[0]}:{peer_address[1]}')
+            handler_thread.start()
+    
+    def run_iteractive_menu(self) -> None:
+        handler_thread = self.IteractiveMenuThread(self)
+        handler_thread.start()
+    
+    # classe aninhada para executar o menu iterativo
+    class IteractiveMenuThread(Thread):
+        def __init__(self, client) -> None:
+            Thread.__init__(self)
+            self._client = client
+
+        # region getters
+        @property
+        def client(self):
+            return self._client
+        
+        # endregion
+
+        # override na função run da thread para execução do menu iterativo
+        def run(self):
+            while True:
+                option = int(input('1 - JOIN | 2 - SEARCH | 3 - DOWNLOAD: '))
+                if option == 1:
+                    self.client.join()
+                elif option == 2:
+                    self.client.search()
+                elif option == 3:
+                    self.client.download()
+                else:
+                    print('Opção inexistente.\n')
+
+    # classe aninhada para tratar as solicitações de download despachadas
+    class DownloadRequestHandlerThread(Thread):
+        def __init__(self, client, peer_socket: socket.socket, peer_address: str) -> None:
+            Thread.__init__(self)
+            self._client = client
+            self._peer_socket = peer_socket
+            self._peer_address = peer_address
+
+        # region getters
+        @property
+        def client(self):
+            return self._client
+        
+        @property
+        def peer_socket(self):
+            return self._peer_socket
+        
+        @property
+        def peer_address(self):
+            return self._peer_address
+        # endregion
+
+        def run(self):
+            print(f'Enviando o arquivo xxxxx para {self.peer_address}...')
+
+
+def main(dev_enviroment = False):
+    #TODO: validar
+    ip = input('IP: ')
+    if dev_enviroment:
+        ip = ip if ip != '' else '127.0.0.1'
+    
+    port = int(input('Port: '))    
+    path = input('Path: ')
+    
+    client = Client(ip, port, path)
+    client.run_iteractive_menu()
+    client.listen_download_requests()
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,16 +1,19 @@
-import socket
+from socket import socket, AF_INET, SOCK_STREAM
 from dataclasses import dataclass
+from threading import Thread
 from mysocket import receive_all
 import command as cmd
 
 @dataclass
 class Server:
+    # construtor da classe server que recebe a parametrizacao do endereço ip:porta vinculado a instancia em execução
     def __init__(self, ip: str, port: int) -> None:
         self._ip = ip
         self._port = port
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind((self.ip, self.port))
-        self.socket.listen(5)
+        # criação do socket de conexão e bind no ip:porta parametrizado
+        self._server_socket = socket(AF_INET, SOCK_STREAM)
+        self._server_socket.bind((self.ip, self.port))
+        self._server_socket.listen(5)
 
     #region getters
     @property
@@ -22,33 +25,58 @@ class Server:
         return self._port
 
     @property
-    def socket(self):
-        return self._socket
+    def server_socket(self):
+        return self._server_socket
     #endregion
+
     def close(self) -> None:
-        self.socket.close()
+        self.server_socket.close()
 
     def listen(self) -> None:
-        print(f"Servidor iniciado em {self.ip}:{self.port}")
         while True:
-            client_socket, client_address = self.socket.accept()
-            print(f"Got connection from: {client_address[0]}:{client_address[1]}")
-            self.handle_request(client_socket)
+            # aguardo um client se conectar
+            client_socket, client_address = self.server_socket.accept()
+            # despacho para um thread tratar sua requisição
+            handler_thread = self.RequestHandlerThread(client_socket, client_address)
+            handler_thread.start()
 
-    def handle_request(self, client_socket) -> None:
-        request = receive_all(client_socket)
-        response = self.process_request(request)
-        if response != '':
-            client_socket.sendall(response.encode())
-        # client_socket.close()
-    
-    def process_request(self, request: bytes) -> str:
-        return cmd.handle(request.decode())
+    # classe aninhada para fazermos o dispatch da requisição para outras threads
+    class RequestHandlerThread(Thread):
+        def __init__(self, client_socket, client_address) -> None:
+            Thread.__init__(self)
+            self._client_socket = client_socket
+            self._client_address = client_address
+      
+        # region getters
+        @property
+        def client_socket(self):
+            return self._client_socket
+        
+        @property
+        def client_address(self):
+            return self._client_address
+        # endregion
 
+        # sobrescrevendo a função run
+        def run(self):
+            request = receive_all(self.client_socket)
+            response = self.process_request(request)
+            if response != '':
+                self.client_socket.sendall(response.encode())
+            self.client_socket.close()
+
+        # aciona o command handler para dar o tratamento adequado de acordo com o comando recebido
+        def process_request(self, request: bytes) -> str:
+            return cmd.handle(request.decode())
 
 def main():
-    server = Server('127.0.0.1', int(input('port:')))
-    try: 
+    try:
+        ip = input('IP (default 127.0.0.1): ')
+        port = input('Port (default 1099): ')
+        ip = ip if ip != '' else '127.0.0.1'
+        port = int(port) if port != '' else 1099
+        server = Server(ip, port)
+
         server.listen()
     finally:
         server.close()
